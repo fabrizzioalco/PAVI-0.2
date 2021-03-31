4ef103909fdc1617e0366c85e2c06a4400f489f6
fabrizzioalco <fabrizzio166@gmail.com>
2021-03-30 23:03:23 -0600

    Adding get request for render json

--- src/Components/VideoList.tsx
+++ src/Components/VideoList.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Typography } from '@material-ui/core';
+import { getJSDocAugmentsTag } from 'typescript';
 
 const Videos = () => {
-    axios.defaults.baseURL ="127.0.0.1:8001";
-    // const getData = () => {
-    //     axios.get('http://127.0.0.1:8001/results/0a0692e6-63e4-11eb-a663-51115c07f023')
-    //         .then((e) => console.log("This is a succes" + e))
-    //         .then((error) => console.log("This is an error:" + error))
-    // }
-    const video = ["video 1", "video 2", "video 3", "video 4", "video 5", "video 6"];
-     const headers = {
+    // axios.defaults.baseURL = "127.0.0.1:8001";
+    const [data, setData] = useState(undefined)
+    const headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         "Access-Control-Allow-Origin": "*"
-    };
+    }
+
+    useEffect(()=>{
+        axios.get('https://127.0.0.1:8001/results/0a0692e6-63e4-11eb-a663-51115c07f023', { headers })
+        .then(response => {
+            setData(response.data)
+            alert("Success" + data)
+        })
+        .catch(error => alert(error)
+    )})
+    // function getData() {
+
+    //     console.log("Im able to access here")
+    //     axios.get('https://127.0.0.1:8001/results/0a0692e6-63e4-11eb-a663-51115c07f023', { headers })
+    //         .then(response => {
+    //             setData(response.data)
+    //             alert("Success" + data)
+    //         })
+    //         .catch(error => alert(error))
+    //     return "I can get here"
+    // }
     return (
         <div>
-            <Typography>
-                {axios.get('http://127.0.0.1:8001/results/0a0692e6-63e4-11eb-a663-51115c07f023', {headers})
-                    .then((e) => console.log("This is a succes" + e))
-                    .then((error) => console.log("This is an error:" + error))}
-            </Typography>
+            <Typography>{data}</Typography>
         </div>
     )
 }
